bc67db515ff3752189e64b37d7ddd8eb6a5c691f
sunil sain <sunilsain00912@gmail.com>
2025-08-30 13:49:28 +0530

    upadte authcontroller

--- mern-auth-main/backend/controllers/authController.js
+++ mern-auth-main/backend/controllers/authController.js
@@ -32,8 +32,8 @@ export const register = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true, // Always true in deployment
+      sameSite: "none", // Important for frontend-backend different domains
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
